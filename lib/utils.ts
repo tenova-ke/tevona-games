53ebeac0a52885975b7f70bace23a1fdd8cc9f3d
tenova-ke <wangatracker@gmail.com>
2025-09-30 00:21:49 +0300

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,9 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 /**
- * Utility function to merge Tailwind + clsx classnames safely
+ * Utility function to merge Tailwind + conditional classes
  */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export function cn(...inputs: (string | undefined | null | false)[]) {
+  return twMerge(clsx(inputs));
+    }
