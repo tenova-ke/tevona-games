58e295c847a10ca5fb9d763cfecb8e1794379f39
tenova-ke <wangatracker@gmail.com>
2025-10-01 12:29:42 +0300

    Update route.ts

--- app/api/coins/transaction/route.ts
+++ app/api/coins/transaction/route.ts
@@ -1,6 +1,6 @@
 // app/api/coins/transaction/route.ts
 import { NextResponse } from "next/server";
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/lib/supabaseClient";
 
 export async function POST(req: Request) {
   const supabase = createClient();
